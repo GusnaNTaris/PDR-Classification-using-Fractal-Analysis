db06314ff4ccf3d7d3d7a011c12f6f16c1b3cc8b
Gusna Taris <taris2798@gmail.com>
2021-03-22 15:21:01 +0700

    Add files via upload

---
+++ resize/resize.m
@@ -0,0 +1,25 @@
+%%Resize gambar segmentasi jadi 909 x XXX pixels
+
+% Specify the folder where the files live.
+myFolder = 'F:\mvessel\downscale\700px';
+% Check to make sure that folder actually exists.  Warn user if it doesn't.
+if ~isfolder(myFolder)
+    errorMessage = sprintf('Error: The following folder does not exist:\n%s\nPlease specify a new folder.', myFolder);
+    uiwait(warndlg(errorMessage));
+    myFolder = uigetdir(); % Ask for a new one.
+    if myFolder == 0
+         % User clicked Cancel
+         return;
+    end
+end
+% Get a list of all files in the folder with the desired file name pattern.
+filePattern = fullfile(myFolder, '*.tif'); % Change to whatever pattern you need.
+theFiles = dir(filePattern);
+for k = 1 : length(theFiles)
+    baseFileName = theFiles(k).name;
+    fullFileName = fullfile(theFiles(k).folder, baseFileName);
+    fprintf(1, 'Now reading %s\n', fullFileName);
+    imageArray = imread(fullFileName);
+    imageResize = imresize(imageArray, [NaN,909]);
+    imwrite(imageResize, baseFileName, 'tiff');  % Save image
+end
